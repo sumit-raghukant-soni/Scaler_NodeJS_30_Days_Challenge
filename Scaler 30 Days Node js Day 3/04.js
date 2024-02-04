044e18204c7ce10b0cd25af02c5737826c4ecac7
Sumit Soni <128374634+sumit-raghukant-soni@users.noreply.github.com>
2024-02-04 23:53:34 +0530

    Delete Scaler 30 Days Node js Day 3/04.js

--- Scaler 30 Days Node js Day 3/04.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-function resolvePath(relativePath) {
-    console.log(path.resolve(relativePath));
-}
-
-// Test cases:
-resolvePath('./test-files/file.txt');
-resolvePath('./non-existent-files/output.txt');
